f20b356bfd87a38aab258c3f95da3e25565e03bc
Emilyqp20 <emilyquezada1@hotmail.com>
2024-10-18 01:55:57 -0500

    Update funciones.h

---
+++ funciones.h
@@ -0,0 +1,6 @@
+void ingresarProductos(char productos[10][50], float precios[10], int cantidad);
+float calcularPrecioTotal(float precios[10], int cantidad);
+void encontrarProductoMasCaro(float precios[10], char productos[10][50], int cantidad);
+void encontrarProductoMasBarato(float precios[10], char productos[10][50], int cantidad);
+float calcularPrecioPromedio(float precios[10], int cantidad);
+void buscarProducto(char productos[10][50], float precios[10], int cantidad, char nombre[50]);
