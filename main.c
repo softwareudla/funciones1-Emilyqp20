aa60052b554bfdf1dac6cc5e7446b46b205d8f53
Emilyqp20 <emilyquezada1@hotmail.com>
2024-10-18 01:55:23 -0500

    Update main.c

---
+++ main.c
@@ -0,0 +1,45 @@
+#include "funciones.h"
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char productos[10][50];
+    float precios[10];
+    int cantidad;
+
+
+    // Solicitar la cantidad de productos y validar con do-while
+    do {
+        printf("¿Cuántos productos quieres ingresar? (1 a 10): ");
+        int validacion = scanf("%d", &cantidad);
+        if (validacion != 1 || cantidad < 1 || cantidad > 10) {
+            printf("Error: Solo puedes ingresar un número entero entre 1 y 10. Intenta de nuevo.\n");
+            while (getchar() != '\n');  // Limpiar el buffer de entrada
+        }
+    } while (cantidad < 1 || cantidad > 10);
+
+    ingresarProductos(productos, precios, cantidad);
+
+    float total = calcularPrecioTotal(precios, cantidad);
+    printf("Precio total del inventario: %.2f\n", total);
+
+    encontrarProductoMasCaro(precios,productos,cantidad);
+    encontrarProductoMasBarato(precios,productos,cantidad);
+
+    float promedio = calcularPrecioPromedio(precios, cantidad);
+    printf("El precio promedio es: %.2f\n", promedio);
+
+    char nombreProducto[50];
+    printf("Ingrese el nombre del producto a buscar: ");
+    getchar(); // Limpiar el buffer antes de fgets
+    fgets(nombreProducto, 50, stdin);
+    nombreProducto[strcspn(nombreProducto, "\n")] = '\0'; // Eliminar el salto de línea
+    buscarProducto(productos, precios, cantidad, nombreProducto);
+
+    // Pausa para esperar a que el usuario presione Enter antes de salir
+    printf("Presione Enter para salir...\n");
+    getchar();  // Captura el Enter que quedó en el buffer
+    getchar();  // Espera a que el usuario presione Enter
+
+    return 0;
+}
